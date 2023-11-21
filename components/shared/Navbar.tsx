27b54b6b42cb7a401f37601e0e0a7a338144e7ec
h-wasi <akhsaikas@gmail.com>
2023-11-21 19:25:16 +0800

    static Navbar added (ring removal pending ⛔⛔⛔)

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -1,37 +1,50 @@
 import Link from "next/link";
 import React from "react";
-import { Button } from "../ui/button";
-// import { Input } from "../ui/input";
+import { Input } from "../ui/input";
 import Image from "next/image";
 
 function Navbar() {
   return (
-    <div className="flex-between">
-      <div className="logo h1-bold">DaysShop</div>
-      <div className={"flex-between justify-between"}>
+    <div className="border-b-light-200 flex justify-around border py-4">
+      <Link href={"/"} className="logo h1-bold">
+        <span className="text-secondary-500">Days</span>
+        <span className="">Shop</span>
+      </Link>
+      <div className={"flex justify-between gap-12 pt-2 text-lg"}>
         <Link href={"/"}>Home</Link>
         <Link href={"/contact"}>Contact</Link>
         <Link href={"/about"}>About</Link>
         <Link href={"/sign-up"}>Sign-Up</Link>
       </div>
-      <div>
-        search
-        <Button asChild>
+      <div className="flex content-between gap-4">
+        <div className="relative flex min-h-[40px] grow items-center gap-1 rounded-xl bg-slate-200 px-4">
+          <Input
+            type="text"
+            placeholder="Search"
+            // value={""}
+            // onChange={() => {}}
+            className="border-0 bg-red-500 outline-none ring-0"
+          />
           <Image
-            src={"/heart-svgrepo-com.svg"}
-            width={16}
-            height={16}
-            alt={"wishlist"}
+            src={"/assets/search.svg"}
+            width={24}
+            height={24}
+            alt="search"
+            className="cursor-pointer "
           ></Image>
-        </Button>
-        <Button asChild>
-          <Image
-            src={"/heart-svgrepo-com.svg"}
-            width={16}
-            height={16}
-            alt={"cart"}
-          ></Image>
-        </Button>
+        </div>
+        <Image
+          src={"/assets/heart.svg"}
+          width={20}
+          height={20}
+          alt="wishlist"
+        ></Image>
+        <Image
+          src={"/assets/cart.svg"}
+          width={20}
+          height={20}
+          alt="cart"
+        ></Image>
       </div>
     </div>
   );
